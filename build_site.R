240ffb1344f1012288139f3c19e5bab94d265eba
erwhite1 <eastonrwhite@gmail.com>
2018-12-11 18:08:11 -0500

    first website commit

---
+++ build_site.R
@@ -0,0 +1,11 @@
+
+
+
+#Set our working directory.
+#This helps avoid confusion if our working directory is
+#not our site because of other projects we were
+#working on at the time.
+setwd("/Users/eastonwhite/Desktop/School/UVM/QuantReasoning")
+
+#render your sweet site.
+rmarkdown::render_site()
